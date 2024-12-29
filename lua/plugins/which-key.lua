f7c9f5f4bb6ca28e644a20b10a2d6bcde6edf059
George McLachlan <georgemclachlan2@gmail.com>
2024-12-29 18:18:30 +1300

    Misc update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,19 +1,19 @@
 return {
-	"folke/which-key.nvim",
-	enabled = false,
-	opts = {
-		preset = "helix",
-	},
-	config = function()
-		local wk = require("which-key")
+    "folke/which-key.nvim",
+    enabled = false,
+    opts = {
+        preset = "helix",
+    },
+    config = function()
+        local wk = require("which-key")
 
-		wk.add({
-			{ "<leader>l", group = "[l]sp (code)" },
-			-- { "<leader>d", group = "[D]ocument" },
-			-- { "<leader>r", group = "[R]ename" },
-			{ "<leader>f", group = "[f]ind" },
-			-- { "<leader>w", group = "[W]orkspace" },
-			{ "<leader>t", group = "[t]oggle tools" },
-		})
-	end,
+        wk.add({
+            { "<leader>l", group = "[l]sp (code)" },
+            -- { "<leader>d", group = "[D]ocument" },
+            -- { "<leader>r", group = "[R]ename" },
+            { "<leader>f", group = "[f]ind" },
+            -- { "<leader>w", group = "[W]orkspace" },
+            { "<leader>t", group = "[t]oggle tools" },
+        })
+    end,
 }
